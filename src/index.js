1cc9cc94d5e388c8ad831f4edf54f4b1daf271a6
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-12 23:40:54 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const yonahPicSrc =
+  "https://static.wikia.nocookie.net/nier/images/f/fc/Project_Gestalt_Report_11242_New.png";
+const devolaAndPopolaPicSrc =
+  "https://cdnb.artstation.com/p/assets/images/images/009/133/185/medium/alexandr-ugryumov-1-2.jpg?1517577325";
+const ninesand2bPicSrc =
+  "https://images8.alphacoders.com/922/thumb-1920-922064.jpg";
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Mystery
+    </h1>
+    <div>
+      <img alt="yonah" src={yonahPicSrc} />
+      <img alt="devola" src={devolaAndPopolaPicSrc} />
+      <img alt="2b" src={ninesand2bPicSrc} />
+    </div>
   </div>,
   document.getElementById("root")
 );
